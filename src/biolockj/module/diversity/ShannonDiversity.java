a060cb41892a5944c381fbeff9404938b7fe629b
Anthony Fodor <anthony.fodor@gmail.com>
2020-05-14 15:56:10 -0400

    skeleton commit

---
+++ src/biolockj/module/diversity/ShannonDiversity.java
@@ -0,0 +1,15 @@
+package biolockj.module.diversity;
+
+import biolockj.module.JavaModuleImpl;
+
+public class ShannonDiversity extends JavaModuleImpl
+{
+
+	@Override
+	public void runModule() throws Exception
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
