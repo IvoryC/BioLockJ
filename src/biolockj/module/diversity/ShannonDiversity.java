e1d4dc56186ae1a735c9f235d69216c1eafdaaff
Anthony Fodor <anthony.fodor@gmail.com>
2020-05-14 23:51:35 -0400

    Shannon diversity working but not tested

--- src/biolockj/module/diversity/ShannonDiversity.java
+++ src/biolockj/module/diversity/ShannonDiversity.java
@@ -1,6 +1,12 @@
 package biolockj.module.diversity;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
+
 import biolockj.module.JavaModuleImpl;
+import biolockj.util.OtuWrapper;
 
 public class ShannonDiversity extends JavaModuleImpl
 {
@@ -8,7 +14,35 @@ public class ShannonDiversity extends JavaModuleImpl
 	@Override
 	public void runModule() throws Exception
 	{
-		// TODO Auto-generated method stub
+		System.out.println("Hello world from Shannon Diversity!!!!");
+		
+		List<File> inputFiles = getInputFiles();
+		
+		for(File f : inputFiles)
+		{
+			System.out.println("Hello "+ f.getAbsolutePath());
+			OtuWrapper wrapper = new OtuWrapper(f);
+
+			File outFile = new File( getOutputDir() + File.separator+ 
+								f.getName().replaceAll(".tsv", "") + "_Shannon.tsv");
+			
+			System.out.println("Hello out file " + outFile.getAbsolutePath());
+			
+			BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+			
+			writer.write("sample\tShannonDiversity\n");
+			
+			List<String> sampleNames = wrapper.getSampleNames();
+			
+			for(String name : sampleNames)
+			{
+				writer.write(name + "\t" + wrapper.getShannonEntropy(name) + "\n");
+			}
+			
+			writer.flush(); writer.close();
+			
+		}
+		
 		
 	}
 
