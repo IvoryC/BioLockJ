074665fef504de49992c54470a6f65022ec1ede2
Ivory <IvoryEC@gmail.com>
2020-07-10 17:33:02 -0400

    Add option to run build specific image with build script; add notes to help in cleanup

--- resources/docker/docker_build_scripts/buildDockerImages.sh
+++ resources/docker/docker_build_scripts/buildDockerImages.sh
@@ -1,10 +1,21 @@
 #! /bin/bash
 
+# $1 - optional pattern to search among dockerfile names; build only matching dockerfiles.
+# example usage:
+# buildDockerImages.sh
+# or 
+# buildDockerImages.sh controller
+
 VER=$(cat ${BLJ}/.version)
 TAG=${VER//-*}
 DIR=${BLJ}/resources/docker/dockerfiles
 
-FILES=$(cat ${BLJ}/resources/docker/docker_build_scripts/buildOrder.txt)
+if [ ${#1} -gt 0 ]; then
+	FILES=$(cat ${BLJ}/resources/docker/docker_build_scripts/buildOrder.txt | grep $1 )
+else
+	FILES=$(cat ${BLJ}/resources/docker/docker_build_scripts/buildOrder.txt)
+fi
+
 for f in $FILES; do
 	name=${f//.Dockerfile}
 	echo "============================"
@@ -13,3 +24,9 @@ for f in $FILES; do
 	docker tag biolockjdevteam/${name}:${TAG} biolockjdevteam/${name}:${VER} && \
 	docker tag biolockjdevteam/${name}:${TAG} biolockjdevteam/${name}:latest && echo "SUCCESS: $name" 1>&2 || echo "FAILURE: $name" 1>&2
 done
+
+
+# If you encounter this error: no space left on device
+# This cleanup is helpful:
+# docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+
