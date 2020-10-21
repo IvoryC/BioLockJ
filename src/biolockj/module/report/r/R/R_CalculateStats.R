5a2c1575c7cc79d87b5865f8439b98dc6363fd81
Ivory <IvoryEC@gmail.com>
2020-10-21 17:40:56 -0400

    Revise R_CalculteStats.R for readablity and flow of both the code and the logged messages.

--- src/biolockj/module/report/r/R/R_CalculateStats.R
+++ src/biolockj/module/report/r/R/R_CalculateStats.R
@@ -1,50 +1,67 @@
 # Module script for: biolockj.module.report.r.R_CalculateStats
 
-main <- function() {
+main <- function(level, countsFile, metadataFile) {
+   
+   message("Running script for level: ", level)
+   message("Using input counts table: ", countsFile)
+   message("Using metadata table: ", metadataFile)
 
    countTable = readBljTable(countsFile)
-   metaTable = readBljTable(metadataFile)
-
-   if ( is.null(countTable) || is.null(metaTable) ) return( NULL )
+   if ( is.null(countTable) ) stop("There was a problem with reading the input counts table: ", countsFile)
+   message("Read in counts table with [", nrow(countTable), "] rows and [", ncol(countTable), "] columns.")
    
+   metaTable = readBljTable(metadataFile)
+   if ( is.null(metaTable) ) stop("There was a problem with reading the metadata table: ", metadataFile)
+   message("Read in metadata table with [", nrow(metaTable), "] rows and [", ncol(metaTable), "] columns.")
+
    merged = mergeTables(countTable, metaTable)
    
-   numDataCols = ncol(countTable) - ncol(metaTable);
-   measuredFeatures = names(countTable)[1:numDataCols]
+   # all features in the input table
+   measuredFeatures = names(countTable)
+   # all features in the output table.  Ends up being measuredFeatures - (any untestable features).
    reportFeatures = c()
-   basicDF = data.frame(matrix(data=measuredFeatures, nrow=numDataCols, ncol=1 ) )
+   
+   basicDF = data.frame(matrix(data=measuredFeatures, nrow=length(measuredFeatures), ncol=1 ) )
    row.names(basicDF) = measuredFeatures
+   
    parametricPvals = basicDF
    nonParametricPvals = basicDF
    rSquaredVals = basicDF
-   
    adjParPvals = basicDF
    adjNonParPvals = basicDF
    
-   message( "binaryCols: ", getBinaryFields() )
-   message( "nominalCols: ", getNominalFields() )
-   message( "numericCols: ", getNumericFields() )
+   binaryFields = getBinaryFields()
+   message( "binaryFields: ", paste0(binaryFields, collapse=", ") )
+   nominalFields = getNominalFields()
+   message( "nominalFields: ", paste0(nominalFields, collapse=", ") )
+   numericFields = getNumericFields()
+   message( "numericFields: ", paste0(numericFields, collapse=", ") )
 
-   # if r.rareOtuThreshold > 1, cutoffValue is an absolute threshold, otherwise it's a % of countTable rows
    cutoffValue = getProperty("r.rareOtuThreshold", 1)
-   message("Using property r.rareOtuThreshold = ", cutoffValue)
-   if( cutoffValue < 1 ) cutoffValue = cutoffValue * nrow(countTable)
+   if( cutoffValue < 1 ) {
+      message("Because the cutoff < 1, it is interpreted as a fraction of countTable rows.")
+      cutoffValue = cutoffValue * nrow(countTable)
+   } 
    message("cutoffValue = ", cutoffValue)
+   
+   logLevel=getProperty("pipeline.logLevel", "DEBUG")
+   if (logLevel=="DEBUG") message( "Message statements from within for-loops will be included." )
+   else message( "Message statements from within for-loops will be NOT included." )
    
    for( feature in measuredFeatures ){
       if( sum( countTable[,feature] > 0 ) < cutoffValue ) {
          message( c( "Excluding: ", feature, "; only found in ", sum( countTable[,feature] > 0 ), "samples.") )
       }else{
          reportFeatures = c(reportFeatures, feature)
-         if( length( getBinaryFields() ) > 0 ) {
-            for( field in getBinaryFields() ) {
-               message( c( "Calculate pvalues for: ", feature, " ~ ", field ) )
+         if( length( binaryFields ) > 0 ) {
+            for( field in binaryFields ) {
+               if (logLevel=="DEBUG") message( c( "Calculate pvalues for: ", feature, " ~ ", field ) )
                att = as.factor( metaTable[,field] )
                vals = levels( att )
                if( everyGroupHasData( merged, feature, field, vals ) ) { 
                   myLm = lm( countTable[,feature] ~ att, na.action=na.exclude )
                   parametricPvals[feature, field] = t.test( countTable[att==vals[1], feature], countTable[att==vals[2], feature] )$p.value
-                  nonParametricPvals[feature, field] = pvalue( wilcox_test.default( countTable, feature, att, vals ) )
+                  nonParametricPvals[feature, field] = pvalue( do_wilcox_test( countTable, feature, att, vals ) )
                   rSquaredVals[feature, field] = summary( myLm )$r.squared
                } else {
                   parametricPvals[feature, field] = 1
@@ -54,9 +71,9 @@ main <- function() {
             }
          }
 
-         if( length( getNominalFields() ) > 0 ) {
-            for( field in getNominalFields() ) {
-               message( c( "Calculate pvalues for: ", feature, " ~ ", field ) )
+         if( length( nominalFields ) > 0 ) {
+            for( field in nominalFields ) {
+               if (logLevel=="DEBUG") message( c( "Calculate pvalues for: ", feature, " ~ ", field ) )
                att = as.factor( metaTable[,field] )
                vals = levels( att )
                if( everyGroupHasData( merged, feature, field, vals ) ) { 
@@ -73,9 +90,9 @@ main <- function() {
             }
          }
          
-         if( length( getNumericFields() ) > 0 ) {
-            for( field in getNumericFields() ) {
-               message( c( "Calculate pvalues for: ", feature, " ~ ", field ) )
+         if( length( numericFields ) > 0 ) {
+            for( field in numericFields ) {
+               if (logLevel=="DEBUG") message( c( "Calculate pvalues for: ", feature, " ~ ", field ) )
                att = as.numeric( metaTable[,field] )
                parametricPvals[feature, field] = Kendall( countTable[,feature], att)$sl[1]
                nonParametricPvals[feature, field] = cor.test( countTable[,feature], att, na.action=na.exclude )$p.value
@@ -95,27 +112,47 @@ main <- function() {
    nonParametricPvals = nonParametricPvals[reportFeatures,2:length(nonParametricPvals)]
    rSquaredVals = rSquaredVals[reportFeatures,2:length(rSquaredVals)]
 
+   message("")
    message( "Calculate ADJUSTED P_VALS" )
-   adjParDF = data.frame(matrix(data=NA, nrow=nrow(parametricPvals), ncol=ncol(parametricPvals) ), row.names=row.names(parametricPvals) )
-   names(adjParDF) = names(parametricPvals)
-   adjNonParDF = data.frame(matrix(data=NA, nrow=nrow(nonParametricPvals), ncol=ncol(nonParametricPvals) ), row.names=row.names(nonParametricPvals)  )
-   names(adjNonParDF) = names(nonParametricPvals)
+   method = getProperty("r_CalculateStats.pAdjustMethod")
+   scopeName = getProperty("r_CalculateStats.pAdjustScope")
+   scope = getP_AdjustLen(scopeName, reportFeatures)
+   message("P values are ajusted using the [", method, "] method, with scope configured to [", scopeName, "] which means the calculation is done using [n=", scope, "].")
    
-   for( rf in getReportFields() ) {
+   message("creating tables to hold adjusted p-vlues...")
+   adjParDF = data.frame(matrix(data=NA, 
+                                nrow=nrow(parametricPvals), 
+                                ncol=ncol(parametricPvals) ), 
+                         row.names=row.names(parametricPvals) )
+   names(adjParDF) = names(parametricPvals)
+   
+   adjNonParDF = data.frame(matrix(data=NA, 
+                                   nrow=nrow(nonParametricPvals), 
+                                   ncol=ncol(nonParametricPvals) ), 
+                            row.names=row.names(nonParametricPvals)  )
+   names(adjNonParDF) = names(nonParametricPvals)
+
+   message("populating adjusted p-vlue tables...")
+   for( rf in c(binaryFields, nominalFields, numericFields) ) {
       message( "Calculate adjusted pval for report field: ", rf )
-      adjParDF[,rf] = p.adjust( parametricPvals[,rf], method=getProperty("r_CalculateStats.pAdjustMethod"), n=getP_AdjustLen(reportFeatures) )
-      adjNonParDF[,rf] = p.adjust( nonParametricPvals[,rf], method=getProperty("r_CalculateStats.pAdjustMethod"), n=getP_AdjustLen(reportFeatures) )
+      adjParDF[,rf] = p.adjust( parametricPvals[,rf], method=method, n=scope )
+      adjNonParDF[,rf] = p.adjust( nonParametricPvals[,rf], method=method, n=scope )
    }
+   message("Done calculating adjusted p-values!")
 
-   message( "measuredFeatures: ", paste(measuredFeatures,sep=", ") )
-   message( "reportFeatures: ", paste(reportFeatures, sep=", ") )
+   message("")
+   message( "measuredFeatures [",length(measuredFeatures), "]: ", paste0(measuredFeatures, collapse=", ") )
+   message("")
+   message( "reportFeatures [",length(reportFeatures), "]: ", paste0(reportFeatures, collapse=", ") )
+   message("")
 
+   message("Saving tables to module output folder...")
    buildSummaryTables( parametricPvals, level, statsFileSuffix( TRUE, FALSE ) )
    buildSummaryTables( nonParametricPvals, level, statsFileSuffix( FALSE, FALSE ) )
    buildSummaryTables( rSquaredVals, level, statsFileSuffix() )
    buildSummaryTables( adjParDF, level, statsFileSuffix( TRUE ) )
    buildSummaryTables( adjNonParDF, level, statsFileSuffix( FALSE ) )
-   
+   message("Done saving tables!")
 
 }
 
@@ -131,15 +168,15 @@ buildSummaryTables <- function( table, level, fileNameTag ) {
 # TAXA adjusts p-values for a all OTUs and all attributes at 1 taxonomy level
 # ATTRIBUTE adjusts p-values for a all OTUs and all taxonomy levels for 1 attribute
 # GLOBAL adjusts p-values for a all OTUs and all attributes and all taxonomy levels
-getP_AdjustLen <- function( names ) {
-   if( getProperty("r_CalculateStats.pAdjustScope") == "GLOBAL" ) {
-      return( length(names) * length( taxaLevels() ) * length( getReportFields() ) )
-   } else if( getProperty("r_CalculateStats.pAdjustScope") == "ATTRIBUTE" ) {
-      return( length(names) * length( taxaLevels() ) )
-   } else if( getProperty("r_CalculateStats.pAdjustScope") == "TAXA" ) {
-      return( length(names) * length(getReportFields()) )
+getP_AdjustLen <- function( scopeName, testFeatures ) {
+   if( scopeName == "GLOBAL" ) {
+      return( length(testFeatures) * length( taxaLevels() ) * length( getReportFields() ) )
+   } else if( scopeName == "ATTRIBUTE" ) {
+      return( length(testFeatures) * length( taxaLevels() ) )
+   } else if( scopeName == "TAXA" ) {
+      return( length(testFeatures) * length(getReportFields()) )
    } else {
-      return( length(names) )
+      return( length(testFeatures) )
    } 
 }
 
@@ -159,7 +196,7 @@ everyGroupHasData <- function( merged, feature, field, vals )  {
 # Method wilcox_test is from the coin package
 # Calculates exact p-values without using heuristic algorithm for better precision
 # Otherwise if ties are found the script may fail
-wilcox_test.default <- function( countTable, feature, att, vals ) {
+do_wilcox_test <- function( countTable, feature, att, vals ) {
    x = countTable[att==vals[1], feature]
    y = countTable[att==vals[2], feature]
    values = c(x, y)
@@ -167,36 +204,21 @@ wilcox_test.default <- function( countTable, feature, att, vals ) {
    return( wilcox_test( values ~ group ) )
 }
 
-# Main function imports coin and Kendall libraries
-# Generates the reportStats list with p-value and R^2 metrics
-# # Outputs summary tables for each metric at each taxonomyLevel
-# main <- function() {
-#    reportStats = calculateStats( level )
-#    if( is.null( reportStats ) ) {
-#       message( c( level, "table is empty" ) )
-#    } else {
-#       message( "Building summary Tables ... " )
-#       buildSummaryTables( reportStats, level )
-#    }
-#    message( "Done!" )
-# }
-
-
 message("Running R version: ", R.Version()$version.string)
 message("Current Working directory: ",getwd())
 
 args = commandArgs(trailingOnly = TRUE)
 message("all args: ", paste(args,sep=", ") )
-level = args[1]
-message("Running script for level: ", level)
-countsFile = args[2]
-message("Using input counts table: ", countsFile)
-metadataFile = args[3]
-message("Using metadata table: ", metadataFile)
 
 source("../resources/BioLockJ_Lib.R")
 importLibs( c( "coin", "Kendall" ) )
 
-main()
+main(level = args[1],
+     countsFile = args[2],
+     metadataFile = args[3])
+
+message("")
+message("Done!")
+message("")
 
 sessionInfo()
