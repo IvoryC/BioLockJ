e5935fa5d26eb8b07bea55079a28efc96993d0a2
Ivory <IvoryEC@gmail.com>
2020-12-02 23:09:58 -0500

    Smooth out the moduleIO infrastructure; DataUnitFactory is parameterized at the methods, not the class level.

--- src/biolockj/dataType/BasicDataUnit.java
+++ src/biolockj/dataType/BasicDataUnit.java
@@ -1,26 +1,17 @@
 package biolockj.dataType;
 
-import java.io.File;
-import java.util.Collection;
-import java.util.List;
-import biolockj.exception.ModuleInputException;
-
 /**
  * Basic implementation to allow a a single class to implement both DataUnit and DataUnitFactory.
  * @author Ivory Blakley
+ * @param <T>
  *
  * @param <T>
  */
-public abstract class BasicDataUnit extends DataUnitImpl implements DataUnitFactory<DataUnit> {
+public abstract class BasicDataUnit extends DataUnitImpl implements DataUnitFactory {
 
 	@Override
-	public Collection<DataUnit> getData( List<File> files, DataUnit template ) throws ModuleInputException {
-		return DataUnitFactory.super.getData( files, (DataUnit) this );
+	public DataUnitFactory getFactory() {
+		return (DataUnitFactory) this;
 	}
-
-	@Override
-	public DataUnitFactory<DataUnit> getFactory() {
-		return this;
-	}
-
+	
 }
