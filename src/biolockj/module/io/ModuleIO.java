9a5e398d2ec50a963c648942f981a1d907546828
Ivory <IvoryEC@gmail.com>
2020-12-05 00:23:05 -0500

    Add a default getInputFiles() method in ModuleIO; demonstrate its use in R_CalcStats

--- src/biolockj/module/io/ModuleIO.java
+++ src/biolockj/module/io/ModuleIO.java
@@ -1,7 +1,11 @@
 package biolockj.module.io;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import biolockj.dataType.DataUnit;
 import biolockj.exception.BioLockJException;
+import biolockj.exception.ModuleInputException;
 import biolockj.module.BioModule;
 import biolockj.util.ModuleUtil;
 
@@ -26,6 +30,25 @@ public interface ModuleIO extends BioModule {
 	 */
 	public List<ModuleOutput> getOutputTypes();
 	
-	
+	/**
+	 * This does not override {@link biolockj.module.BioModuleImpl#getInputFiles()}.
+	 * But it may eventually replace it.  And in the mean time, it is a convenient reference while migrating modules.
+	 */
+	@Override
+	public default List<File> getInputFiles() throws ModuleInputException {
+		final List<File> files = new ArrayList<>();
+		final List<DataUnit> data = new ArrayList<>();
+		try {
+			for( ModuleInput input: getInputTypes() ) {
+				data.addAll( input.getSource().getData() );
+			}
+			for( DataUnit du: data ) {
+				files.addAll( du.getFiles() );
+			}
+		} catch( Exception e ) {
+			throw new ModuleInputException( this, e );
+		}
+		return files;
+	}
 
 }
