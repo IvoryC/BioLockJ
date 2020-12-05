22b8e40e8596d6382c744a572e1a4bd5b6a376a7
Ivory <IvoryEC@gmail.com>
2020-12-05 02:09:57 -0500

    Expand the meta data field classes; add CountField and OtuCountField.

---
+++ src/biolockj/module/report/otu/CountField.java
@@ -0,0 +1,41 @@
+package biolockj.module.report.otu;
+
+import java.util.List;
+import org.apache.commons.lang.math.NumberUtils;
+import biolockj.dataType.NumericMetaData;
+
+/**
+ * An extension of {@link NumericMetaData} that additionally requires that the column include only count data, that is,
+ * all values are integers >=0.
+ * 
+ * @author Ivory Blakley
+ *
+ */
+public class CountField extends NumericMetaData {
+
+	public CountField( String name ) {
+		super( name );
+	}
+
+	@Override
+	public String getDescription() {
+		return "A metadata attribute with counts data. All non-null values are integers >= 0.";
+	}
+	
+	@Override
+	protected boolean verifyVals(List<String> vals) {
+		if (vals.isEmpty()) return false;
+		boolean allNumbers = true;
+		for (String val : vals) {
+			try {
+				allNumbers = allNumbers && NumberUtils.createInteger( val ) >= 0;
+			}catch(NumberFormatException nfe) {
+				allNumbers = false;
+				break;
+			}
+			if (!allNumbers) break;
+		}
+		return allNumbers;
+	}
+
+}
