aa6aea9f06ccaefeeb2b83d87fa2a1fe8e744e83
Ivory <IvoryEC@gmail.com>
2020-12-09 17:06:42 -0500

    Simplify the GenericSeqData class by adding defaults in SequenceAlphabet and SequenceFormat.

--- src/biolockj/dataType/seq/SequenceAlphabet.java
+++ src/biolockj/dataType/seq/SequenceAlphabet.java
@@ -1,7 +1,34 @@
-package biolockj.dataTypes.seq;
+package biolockj.dataType.seq;
 
+/**
+ * Represents the alphabet of sequence data.
+ * @author Ivory Blakley
+ *
+ */
 public interface SequenceAlphabet {
 	
-	char[] getCharacters();
-
+	/**
+	 * Get the set of characters in the alphabet.
+	 * @return
+	 */
+	public default char[] getCharacters() {
+		// An all-chars default; 
+		// this allows for implementing class that have an alphabet, but open-ended and/or not known.
+		return "abcdefjhijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-.?1234567890*".toCharArray();
+	}
+	
+	/**
+	 * Is a given character a member of this alphabet ?
+	 * @param c
+	 * @return
+	 */
+	public default boolean isMember(char c) {
+		boolean isMember = false;
+		for (char member : getCharacters() ) if ( c == member ) {
+			isMember = true;
+			break;
+		}
+		return isMember;
+	}
+	
 }
